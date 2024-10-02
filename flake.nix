c1be337fba88bb62100b8602630d28826b516571
Ethan Rooke <ethan@roo.ke>
2024-10-02 17:36:09 -0500

    🎉 initial commit

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "OCP Ecosystem packaged for nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      formatter.${system} = pkgs.nixfmt-rfc-style;
+    };
+}
