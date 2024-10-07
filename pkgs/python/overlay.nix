0b42e0ce8d31179076a3e6f95197f8f496a50553
Ethan Rooke <ethan@roo.ke>
2024-10-06 20:54:56 -0500

    nlopt: init python package
    Signed-off-by: Ethan Rooke <ethan@roo.ke>

--- pkgs/python/overlay.nix
+++ pkgs/python/overlay.nix
@@ -3,6 +3,7 @@
 }:
 final: prev: {
   libclang = prev.libclang.override { llvmPackages = llvmPackages_15; };
+  nlopt = final.callPackage ./nlopt { };
   pywrap = final.callPackage ./pywrap { };
   svgpathtools = final.callPackage ./svgpathtools { };
   trianglesolver = final.callPackage ./trianglesolver { };
