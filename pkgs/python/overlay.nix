9889888ba6f22fdc66b1f98d1a51285acc8733cc
Ethan Rooke <ethan@roo.ke>
2024-10-06 20:55:39 -0500

    casadi: init python package
    Signed-off-by: Ethan Rooke <ethan@roo.ke>

--- pkgs/python/overlay.nix
+++ pkgs/python/overlay.nix
@@ -1,7 +1,9 @@
 {
+  casadi,
   llvmPackages_15,
 }:
 final: prev: {
+  casadi = final.toPythonModule (casadi.override { pythonSupport = true; });
   libclang = prev.libclang.override { llvmPackages = llvmPackages_15; };
   nlopt = final.callPackage ./nlopt { };
   pywrap = final.callPackage ./pywrap { };
