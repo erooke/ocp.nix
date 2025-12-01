31ba7c7bc455207c7420c8ba812375d7a08bec70
Ethan Rooke <ethan@roo.ke>
2025-12-01 14:18:04 -0600

    ocpsvg 0.2.1 -> 0.5.0

--- pkgs/python/ocpsvg/default.nix
+++ pkgs/python/ocpsvg/default.nix
@@ -7,15 +7,14 @@
   setuptools-scm,
   # Runtime dependencies
   ocp,
-  svgpathtools,
   svgelements,
 }:
 let
   pname = "ocpsvg";
-  version = "0.2.1";
+  version = "0.5.0";
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-igiSSbUrC/+ZzKlpivPXTvZ4ztI1kxb4wqBK7puq+0Y=";
+    hash = "sha256-XNjb7Iv1kNNzqCquvqskGDgYWqsE7ihZ8zudeVa7+6Y=";
   };
 in
 buildPythonPackage {
@@ -36,7 +35,6 @@ buildPythonPackage {
 
   dependencies = [
     ocp
-    svgpathtools
     svgelements
   ];
 
